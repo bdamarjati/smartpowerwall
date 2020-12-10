ebac3db9963d98ac08622229790b164f21a8f16e
wkhid23 <33.muhammadwakhid@gmail.com>
2020-12-10 14:01:48 +0700

    wakhid

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Auth::routes();
+
 Route::get('/', function () {
-    return view('dashboard');
+    return view('auth.login');
 });
+
+Route::get('/logout', function(){
+    Auth::logout();
+    return Redirect::to('/');
+});
+
+// Route::get('/changePassword','ChangePasswordController@showChangePasswordForm');
+Route::get('/changePassword', [App\Http\Controllers\ChangePasswordController::class, 'showChangePasswordForm'])->name('changePassword');
+Route::post('/changePassword',[App\Http\Controllers\ChangePasswordController::class, 'changePassword'])->name('changePassword');
+
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
