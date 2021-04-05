578cd4649c8f68650c3c4e8b4886a638dc4810aa
bdamarjati <bimadamar13@outlook.com>
2021-04-05 14:03:43 +0700

    Add Inverter Monitoring Page

---
+++ public/assets/js/pages/inverter.js
@@ -0,0 +1,539 @@
+try {
+
+    Apex.tooltip = {
+        theme: 'dark'
+    }
+
+    // Usage
+    var usage = [16600, 16800, 15500, 17800, 15500, 17000, 19000, 16000, 15000, 17000, 14000, 17000];
+    var charge = [16500, 17500, 16200, 17300, 16000, 19500, 16000, 17000, 16000, 19000, 18000, 19000];
+    var month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+    //Updating Chart
+    var Toggle = setInterval(UpdateChart, 3000);
+
+    function UpdateChart() {
+        var newusage = Math.floor(Math.random() * 20000) + 10000;
+        var newcharge = Math.floor(Math.random() * 20000) + 10000;
+        var newload = Math.floor(Math.random() * 20000) + 10000;
+
+        usage.shift();
+        usage.push(newusage);
+        charge.shift();
+        charge.push(newcharge);
+
+        var thismonth = month[0];
+        month.shift();
+        month.push(thismonth);
+
+        chart1.updateOptions({
+            series: [{
+                name: 'Usages',
+                data: usage
+            }],
+        });
+
+        chart2.updateOptions({
+            series: [{
+                name: 'Usages',
+                data: usage
+            }],
+        });
+
+        chart3.updateOptions({
+            series: [{
+                name: 'Usages',
+                data: usage
+            }],
+        });
+
+        chart.updateOptions({
+            series: [newusage, newcharge, newload]
+        });
+    }
+
+    var options1 = {
+        chart: {
+            fontFamily: 'Nunito, sans-serif',
+            height: 365,
+            type: 'area',
+            zoom: {
+                enabled: false
+            },
+            dropShadow: {
+                enabled: true,
+                opacity: 0.3,
+                blur: 5,
+                left: -7,
+                top: 22
+            },
+            toolbar: {
+                show: false
+            },
+            events: {
+                mounted: function (ctx, config) {
+                    const highest1 = ctx.getHighestValueInSeries(0);
+                    const highest2 = ctx.getHighestValueInSeries(1);
+                },
+            }
+        },
+        colors: ['#009688'],
+        dataLabels: {
+            enabled: false
+        },
+        markers: {
+            discrete: [{
+                seriesIndex: 0,
+                dataPointIndex: 7,
+                fillColor: '#000',
+                strokeColor: '#000',
+                size: 5
+            }]
+        },
+        stroke: {
+            show: true,
+            curve: 'smooth',
+            width: 2,
+            lineCap: 'square'
+        },
+        series: {
+            name: 'Usages',
+            data: usage
+        },
+        labels: month,
+        xaxis: {
+            axisBorder: {
+                show: false
+            },
+            axisTicks: {
+                show: false
+            },
+            crosshairs: {
+                show: true
+            },
+            labels: {
+                offsetX: 0,
+                offsetY: 5,
+                style: {
+                    fontSize: '12px',
+                    fontFamily: 'Nunito, sans-serif',
+                    cssClass: 'apexcharts-xaxis-title',
+                },
+            }
+        },
+        yaxis: {
+            labels: {
+                formatter: function (value, index) {
+                    return (value / 1000) + 'K'
+                },
+                offsetX: -22,
+                offsetY: 0,
+                style: {
+                    fontSize: '12px',
+                    fontFamily: 'Nunito, sans-serif',
+                    cssClass: 'apexcharts-yaxis-title',
+                },
+            }
+        },
+        grid: {
+            borderColor: '#191e3a',
+            strokeDashArray: 5,
+            xaxis: {
+                lines: {
+                    show: true
+                }
+            },
+            yaxis: {
+                lines: {
+                    show: false,
+                }
+            },
+            padding: {
+                top: 0,
+                right: 0,
+                bottom: 0,
+                left: -10
+            },
+        },
+        legend: {
+            position: 'top',
+            horizontalAlign: 'right',
+            offsetY: -50,
+            fontSize: '16px',
+            fontFamily: 'Nunito, sans-serif',
+            markers: {
+                width: 10,
+                height: 10,
+                strokeWidth: 0,
+                strokeColor: '#fff',
+                fillColors: undefined,
+                radius: 12,
+                onClick: undefined,
+                offsetX: 0,
+                offsetY: 0
+            },
+            itemMargin: {
+                horizontal: 0,
+                vertical: 20
+            }
+        },
+        tooltip: {
+            theme: 'dark',
+            marker: {
+                show: true,
+            },
+            x: {
+                show: false,
+            }
+        },
+        fill: {
+            type: "gradient",
+            gradient: {
+                type: "vertical",
+                shadeIntensity: 1,
+                inverseColors: !1,
+                opacityFrom: .28,
+                opacityTo: .05,
+                stops: [45, 100]
+            }
+        },
+        responsive: [{
+            breakpoint: 575,
+            options: {
+                legend: {
+                    offsetY: -30,
+                },
+            },
+        }]
+    }
+
+    var options2 = {
+        chart: {
+            fontFamily: 'Nunito, sans-serif',
+            height: 365,
+            type: 'area',
+            zoom: {
+                enabled: false
+            },
+            dropShadow: {
+                enabled: true,
+                opacity: 0.3,
+                blur: 5,
+                left: -7,
+                top: 22
+            },
+            toolbar: {
+                show: false
+            },
+            events: {
+                mounted: function (ctx, config) {
+                    const highest1 = ctx.getHighestValueInSeries(0);
+                    const highest2 = ctx.getHighestValueInSeries(1);
+                },
+            }
+        },
+        colors: ['#1b55e2'],
+        dataLabels: {
+            enabled: false
+        },
+        markers: {
+            discrete: [{
+                seriesIndex: 0,
+                dataPointIndex: 7,
+                fillColor: '#000',
+                strokeColor: '#000',
+                size: 5
+            }]
+        },
+        stroke: {
+            show: true,
+            curve: 'smooth',
+            width: 2,
+            lineCap: 'square'
+        },
+        series: {
+            name: 'Usages',
+            data: usage
+        },
+        labels: month,
+        xaxis: {
+            axisBorder: {
+                show: false
+            },
+            axisTicks: {
+                show: false
+            },
+            crosshairs: {
+                show: true
+            },
+            labels: {
+                offsetX: 0,
+                offsetY: 5,
+                style: {
+                    fontSize: '12px',
+                    fontFamily: 'Nunito, sans-serif',
+                    cssClass: 'apexcharts-xaxis-title',
+                },
+            }
+        },
+        yaxis: {
+            labels: {
+                formatter: function (value, index) {
+                    return (value / 1000) + 'K'
+                },
+                offsetX: -22,
+                offsetY: 0,
+                style: {
+                    fontSize: '12px',
+                    fontFamily: 'Nunito, sans-serif',
+                    cssClass: 'apexcharts-yaxis-title',
+                },
+            }
+        },
+        grid: {
+            borderColor: '#191e3a',
+            strokeDashArray: 5,
+            xaxis: {
+                lines: {
+                    show: true
+                }
+            },
+            yaxis: {
+                lines: {
+                    show: false,
+                }
+            },
+            padding: {
+                top: 0,
+                right: 0,
+                bottom: 0,
+                left: -10
+            },
+        },
+        legend: {
+            position: 'top',
+            horizontalAlign: 'right',
+            offsetY: -50,
+            fontSize: '16px',
+            fontFamily: 'Nunito, sans-serif',
+            markers: {
+                width: 10,
+                height: 10,
+                strokeWidth: 0,
+                strokeColor: '#fff',
+                fillColors: undefined,
+                radius: 12,
+                onClick: undefined,
+                offsetX: 0,
+                offsetY: 0
+            },
+            itemMargin: {
+                horizontal: 0,
+                vertical: 20
+            }
+        },
+        tooltip: {
+            theme: 'dark',
+            marker: {
+                show: true,
+            },
+            x: {
+                show: false,
+            }
+        },
+        fill: {
+            type: "gradient",
+            gradient: {
+                type: "vertical",
+                shadeIntensity: 1,
+                inverseColors: !1,
+                opacityFrom: .28,
+                opacityTo: .05,
+                stops: [45, 100]
+            }
+        },
+        responsive: [{
+            breakpoint: 575,
+            options: {
+                legend: {
+                    offsetY: -30,
+                },
+            },
+        }]
+    }
+
+    var options3 = {
+        chart: {
+            fontFamily: 'Nunito, sans-serif',
+            height: 365,
+            type: 'area',
+            zoom: {
+                enabled: false
+            },
+            dropShadow: {
+                enabled: true,
+                opacity: 0.3,
+                blur: 5,
+                left: -7,
+                top: 22
+            },
+            toolbar: {
+                show: false
+            },
+            events: {
+                mounted: function (ctx, config) {
+                    const highest1 = ctx.getHighestValueInSeries(0);
+                    const highest2 = ctx.getHighestValueInSeries(1);
+                },
+            }
+        },
+        colors: ['#e7515a'],
+        dataLabels: {
+            enabled: false
+        },
+        markers: {
+            discrete: [{
+                seriesIndex: 0,
+                dataPointIndex: 7,
+                fillColor: '#000',
+                strokeColor: '#000',
+                size: 5
+            }]
+        },
+        stroke: {
+            show: true,
+            curve: 'smooth',
+            width: 2,
+            lineCap: 'square'
+        },
+        series: {
+            name: 'Usages',
+            data: usage
+        },
+        labels: month,
+        xaxis: {
+            axisBorder: {
+                show: false
+            },
+            axisTicks: {
+                show: false
+            },
+            crosshairs: {
+                show: true
+            },
+            labels: {
+                offsetX: 0,
+                offsetY: 5,
+                style: {
+                    fontSize: '12px',
+                    fontFamily: 'Nunito, sans-serif',
+                    cssClass: 'apexcharts-xaxis-title',
+                },
+            }
+        },
+        yaxis: {
+            labels: {
+                formatter: function (value, index) {
+                    return (value / 1000) + 'K'
+                },
+                offsetX: -22,
+                offsetY: 0,
+                style: {
+                    fontSize: '12px',
+                    fontFamily: 'Nunito, sans-serif',
+                    cssClass: 'apexcharts-yaxis-title',
+                },
+            }
+        },
+        grid: {
+            borderColor: '#191e3a',
+            strokeDashArray: 5,
+            xaxis: {
+                lines: {
+                    show: true
+                }
+            },
+            yaxis: {
+                lines: {
+                    show: false,
+                }
+            },
+            padding: {
+                top: 0,
+                right: 0,
+                bottom: 0,
+                left: -10
+            },
+        },
+        legend: {
+            position: 'top',
+            horizontalAlign: 'right',
+            offsetY: -50,
+            fontSize: '16px',
+            fontFamily: 'Nunito, sans-serif',
+            markers: {
+                width: 10,
+                height: 10,
+                strokeWidth: 0,
+                strokeColor: '#fff',
+                fillColors: undefined,
+                radius: 12,
+                onClick: undefined,
+                offsetX: 0,
+                offsetY: 0
+            },
+            itemMargin: {
+                horizontal: 0,
+                vertical: 20
+            }
+        },
+        tooltip: {
+            theme: 'dark',
+            marker: {
+                show: true,
+            },
+            x: {
+                show: false,
+            }
+        },
+        fill: {
+            type: "gradient",
+            gradient: {
+                type: "vertical",
+                shadeIntensity: 1,
+                inverseColors: !1,
+                opacityFrom: .28,
+                opacityTo: .05,
+                stops: [45, 100]
+            }
+        },
+        responsive: [{
+            breakpoint: 575,
+            options: {
+                legend: {
+                    offsetY: -30,
+                },
+            },
+        }]
+    }
+
+    var chart1 = new ApexCharts(
+        document.querySelector("#usageMonthly"),
+        options1
+    );
+    var chart2 = new ApexCharts(
+        document.querySelector("#usageMonthlyB"),
+        options2
+    );
+    var chart3 = new ApexCharts(
+        document.querySelector("#usageMonthlyC"),
+        options3
+    );
+    chart1.render();
+    chart2.render();
+    chart3.render();
+
+    const ps = new PerfectScrollbar(document.querySelector('.mt-container'));
+} catch (e) {
+    console.log(e);
+}
--- resources/views/inverter.blade.php
+++ resources/views/inverter.blade.php
@@ -12,13 +12,77 @@
     <div class="layout-px-spacing">
         <div class="row layout-top-spacing">
 
+            <div class="col-xl-8 col-lg-12 col-md-12 col-sm-12 col-12 layout-spacing">
+                <div class="row widget-statistic">
+
+                    <div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-12">
+                        <div class="widget widget-one_hybrid widget-engagement">
+                            <div class="widget-heading">
+                                <div class="w-icon">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                        fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                        stroke-linejoin="round" class="feather feather-message-circle">
+                                        <!--<path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z"></path>-->
+                                    </svg>
+                                </div>
+                                <p class="w-value" id="textcap">220 Volt</p>
+                                <h5 class="">Voltage</h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-12">
+                        <div class="widget widget-one_hybrid widget-followers">
+                            <div class="widget-heading">
+                                <div class="w-icon">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                        fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                        stroke-linejoin="round" class="feather feather-users">
+                                        <!--<path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M23 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path>-->
+                                    </svg>
+                                </div>
+                                <p class="w-value" id="textcharge">3.30 A</p>
+                                <h5 class="">Current</h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-12">
+                        <div class="widget widget-one_hybrid widget-referral">
+                            <div class="widget-heading">
+                                <div class="w-icon">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                        fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                        stroke-linejoin="round" class="feather feather-link">
+                                        <!--<path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"></path><path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"></path>-->
+                                    </svg>
+                                </div>
+                                <p class="w-value" id="textload">50 Hz</p>
+                                <h5 class="">Frequency</h5>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="col-xl-4 col-lg-12 col-md-12 col-sm-12 col-12 layout-spacing">
+                <div class="widget widget-chart-two">
+                    <div class="widget-heading">
+                        <h5 class="">MODE</h5>
+                    </div>
+                    <div class="widget-content">
+                        <!-- <div id="chart-2" class=""></div> -->
+                        <h3 style="text-align: center; padding: 39px; font-weight: bold;">GRID</h3>
+                    </div>
+                </div>
+            </div>
+
             <div class="col-xl-6 col-lg-12 col-md-12 col-sm-12 col-12 layout-spacing">
                 <div class="widget widget-chart-one">
                     <div class="widget-heading">
-                        <h5 class="">Usages</h5>
-                        <ul class="tabs tab-pills">
+                        <h5 class="">Voltage</h5>
+                        <!-- <ul class="tabs tab-pills">
                             <li><a href="javascript:void(0);" id="tb_1" class="tabmenu">Monthly</a></li>
-                        </ul>
+                        </ul> -->
                     </div>
 
                     <div class="widget-content">
@@ -31,23 +95,43 @@
                 </div>
             </div>
 
-            <!-- <div class="col-xl-4 col-lg-12 col-md-12 col-sm-12 col-12 layout-spacing">
-                <div class="widget widget-four">
+            <div class="col-xl-6 col-lg-12 col-md-12 col-sm-12 col-12 layout-spacing">
+                <div class="widget widget-chart-one">
                     <div class="widget-heading">
-                        <h4 class="font-weight-bold align-left"><b>DEBUG</b>
-                        </h4>
+                        <h5 class="">Current</h5>
+                        <!-- <ul class="tabs tab-pills">
+                            <li><a href="javascript:void(0);" id="tb_1" class="tabmenu">Monthly</a></li>
+                        </ul> -->
                     </div>
+
                     <div class="widget-content">
-                        <h5 class="table-1 align-left" id="debug">TEXT
-
-                        </h5>
-                        </br>
-                        <h5 class="table-1 align-left">
-
-                        </h5>
+                        <div class="tabs tab-content">
+                            <div id="content_2" class="tabcontent">
+                                <div id="usageMonthlyB"></div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div> -->
+            </div>
+
+            <div class="col-xl-6 col-lg-12 col-md-12 col-sm-12 col-12 layout-spacing">
+                <div class="widget widget-chart-one">
+                    <div class="widget-heading">
+                        <h5 class="">Frequency</h5>
+                        <!-- <ul class="tabs tab-pills">
+                            <li><a href="javascript:void(0);" id="tb_1" class="tabmenu">Monthly</a></li>
+                        </ul> -->
+                    </div>
+
+                    <div class="widget-content">
+                        <div class="tabs tab-content">
+                            <div id="content_3" class="tabcontent">
+                                <div id="usageMonthlyC"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
             <div class="col-xl-6 col-lg-12 col-md-12 col-sm-12 col-12 layout-spacing">
                 <div class="widget widget-table-two">
@@ -159,6 +243,6 @@
 
 <!-- BEGIN PAGE LEVEL PLUGINS/CUSTOM SCRIPTS -->
 <script src="plugins/apex/apexcharts.min.js"></script>
-<script src="assets/js/dashboard/dash_2.js"></script>
+<script src="assets/js/pages/inverter.js"></script>
 <!-- BEGIN PAGE LEVEL PLUGINS/CUSTOM SCRIPTS -->
 @endsection
